e4ad0942cb929e33737d84ba98b2c3be9eb3ba2a
toomanychung <toomanychung@gmail.com>
2018-10-03 22:32:54 +0800

    day 5

--- Exercism/javascript/gigasecond/gigasecond.js
+++ Exercism/javascript/gigasecond/gigasecond.js
@@ -1,13 +1,16 @@
-function gigasecond(year,month,day){
-	var birthday = new Date(year,month-1,day) // Month is zero-based
-	var date1 = birthday.getTime();
-	date2 = new Date(date1 + 1000000000000); //10000000000s(10^9 second) = 1000000000000ms
-	// No Deatils time needed
-	date2.setHours(0);
-	date2.setSeconds(0);
-	date2.setMinutes(0);
-	console.log("The day after 10^9 second you borned will be on " + date2);
- }
+	function Gigasecond(birthDate) {
+	  this.birthDate = birthDate;
+	  this.gigasecond = 1000000000000;
+	};
+  
+	Gigasecond.prototype.date = function() {
+
+	  return new Date(this.birthDate.getTime() + this.gigasecond);
+	};
+  
+
+	module.exports = Gigasecond;
+
+	// var gs = new Gigasecond(Date.UTC(2015, 8, 14));
+	// console.log(gs.date());
  
- //Typing the birthday by yyyy-mm-dd order and system shown the moment after 10^9 seconds.
- gigasecond(1993,10,13);
